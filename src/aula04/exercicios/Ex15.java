d4c9a6e3ace7993a8ff6e6413e6b920ed3492abc
rodrigoh <rodrigohenrich@faccat.br>
2025-03-19 10:19:37 -0300

    Ajustes no exercício

--- src/aula04/exercicios/Ex15.java
+++ src/aula04/exercicios/Ex15.java
@@ -33,13 +33,13 @@ public class Ex15 {
 			case 1->{
 				if(a < b && b < c)
 					System.out.println(a+" "+b+" "+c);
-				else if(a < c && c < a)
+				else if(a < c && c < b)
 					System.out.println(a+" "+c+" "+b);
 				else if(b < a && a < c)
 					System.out.println(b+" "+a+" "+c);
 				else if(b < c && c < a)
 					System.out.println(b+" "+c+" "+a);
-				else if(a < a && a < b)
+				else if(c < a && a < b)
 					System.out.println(c+" "+a+" "+b);
 				else if(c < b && b < a)
 					System.out.println(c+" "+b+" "+a);
@@ -47,13 +47,13 @@ public class Ex15 {
 			case 2->{
 				if(a > b && b > c)
 					System.out.println(a+" "+b+" "+c);
-				else if(a > c && c > a)
+				else if(a > c && c > b)
 					System.out.println(a+" "+c+" "+b);
 				else if(b > a && a > c)
 					System.out.println(b+" "+a+" "+c);
 				else if(b > c && c > a)
 					System.out.println(b+" "+c+" "+a);
-				else if(a > a && a > b)
+				else if(c > a && a > b)
 					System.out.println(c+" "+a+" "+b);
 				else if(c > b && b > a)
 					System.out.println(c+" "+b+" "+a);
