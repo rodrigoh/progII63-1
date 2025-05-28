441a1fe8becf4c981e427e3eb54dfc8608576ba9
rodrigoh <rodrigohenrich@faccat.br>
2025-05-28 10:49:49 -0300

    Exemplos herança

--- src/aula13/exemploHeranca/Principal.java
+++ src/aula13/exemploHeranca/Principal.java
@@ -2,6 +2,6 @@ package aula13.exemploHeranca;
 
 public class Principal {
 	public static void main(String[] args) {
-		Motorista m1 = new Motorista();
+		Motorista m1 = new Motorista("Jonas","Silva",123,"c58636");
 	}
 }
